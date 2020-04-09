9ce7ff7a27dafb32f1f5d388b037a41c4aae53fb
aditya wirawan pamungkas <61973446+wirawanpamungkas52@users.noreply.github.com>
2020-04-10 00:07:47 +0700

    Add files via upload

---
+++ 07latihan5.php
@@ -0,0 +1,35 @@
+<HTML>
+<HEAD>
+<TITLE> Penggunaan List </TITLE>
+</HEAD>
+<BODY>
+<?php
+$program = array('Bobo','Doraemon','Spiderman');
+list($Majalah, $Komik, $Film) = $program;
+echo "Jenis Buku & Hiburan :";
+echo "<br />";
+echo "Cerpen : $Majalah";
+echo "<br />";
+echo "Cerita Bergambar : $Komik";
+echo "<br />";
+echo "Bioskop : $Film";
+?>
+</BODY>
+</HTML>
+Latihan 6.
+<HTML>
+<HEAD>
+<TITLE> Penggunaan List 2 </TITLE>
+</HEAD>
+<BODY>
+<?php
+$program = array('Bobo','Doraemon','Spiderman');
+list($Majalah, , $Film) = $program;
+echo "Jenis Buku & Hiburan :";
+echo "<br />";
+echo "Cerpen : $Majalah";
+echo "<br />";
+echo "Bioskop : $Film";
+?> 
+</BODY>
+</HTML>
